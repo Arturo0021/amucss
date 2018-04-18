5fd31b2b748149a5cd40387cf6c8e42dba7c04fc
Ivan Arturo SG <xcode.arturo@gmail.com>
2018-04-18 00:05:29 -0500

    Layout_Registro, Estructura de Carpetas
    Se anexo el layout de registro aun sin funcionalidad y se anexo la estructura de carpetas para programacion en capas

--- app/src/main/java/com/amucss/mx/amucss/MainActivity.java
+++ app/src/main/java/com/amucss/mx/amucss/MainActivity.java
@@ -1,14 +1,19 @@
 package com.amucss.mx.amucss;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt_registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,15 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        txt_registro = (TextView)findViewById(R.id.txt_registro);
 
+        txt_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Registro_Activity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/amucss/mx/amucss/Registro_Activity.java
@@ -0,0 +1,35 @@
+package com.amucss.mx.amucss;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Registro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_registro_);
+
+
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+}
