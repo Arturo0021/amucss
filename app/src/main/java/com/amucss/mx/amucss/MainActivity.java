eeeefe3a7678eb399f7a7513051429e12869c14f
Ivan Arturo SG <xcode.arturo@gmail.com>
2018-04-21 05:26:36 -0500

    Interfaz y Funcion
    Se cambiaron las dos pantallas mas la funcionalidad, agregando nuevas cosas, como dialog para accesos, aviso de privasidad, el layout  de captura esta completo igual el principal

--- app/src/main/java/com/amucss/mx/amucss/MainActivity.java
+++ app/src/main/java/com/amucss/mx/amucss/MainActivity.java
@@ -1,35 +1,122 @@
 package com.amucss.mx.amucss;
 
+
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.ActionBar;
+import android.content.pm.ActivityInfo;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txt_registro;
+    Button Registro; // Registro
+    Button Acces; // Acces con clave
+    Button Español; // Español
+    Button Nahuatl; // Nahuatl
+    Button Otomi; // Otomi
+    Button Mixteco; // Mixteco
+    Button Zapoteco; // Zapoteco
+    FloatingActionButton fl_salir; // Boton salir
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Mantiene la pantalla Full
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Rotacion de pantalla y bloqueo
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // Rota horizontal
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Rota Vertical
+
         setContentView(R.layout.activity_main);
 
-        txt_registro = (TextView)findViewById(R.id.txt_registro);
+        // Tendra un evento de intent
+        Registro = (Button)findViewById(R.id.button1);
+        Acces = (Button)findViewById(R.id.button2);
+        // Estos botones controlan el Idioma
+            Español = (Button)findViewById(R.id.button3);
+            Nahuatl = (Button)findViewById(R.id.button4);
+            Otomi = (Button)findViewById(R.id.button5);
+            Mixteco = (Button)findViewById(R.id.button6);
+            Zapoteco = (Button)findViewById(R.id.button7);
+        // Simplemente Cierra la pantalla
+        fl_salir = (FloatingActionButton)findViewById(R.id.fl_salir);
 
-        txt_registro.setOnClickListener(new View.OnClickListener() {
+        // Envia un intent a la pantalla de Registro
+        Registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Registro_Activity.class);
+                Intent intent = new Intent(MainActivity.this,Registro_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+            // Agregar aqui los demas eventos en orden Salir hasta abajo
+        Acces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("CLAVE");
+                builder.setMessage("Ingrese su Clave");
+
+                final EditText input = new EditText(MainActivity.this);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                builder.setView(input);
+                builder.setIcon(R.drawable.key);
+
+
+
+                builder.setPositiveButton("ACEPTAR",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                if(input.getText().toString().equals("") || input.getText().toString().equals(null)) {
+                                    Toast.makeText(MainActivity.this, "Error Campo Clave Vacia", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, input.getText(), Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+                builder.setNegativeButton("CANCELAR",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                builder.show();
+            }
+
+        });
+
+        fl_salir.setOnClickListener(new View.OnClickListener() { // Boton Salir
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             }
         });
 
     }
+
+
+
 }
--- app/src/main/java/com/amucss/mx/amucss/Registro_Activity.java
+++ app/src/main/java/com/amucss/mx/amucss/Registro_Activity.java
@@ -1,35 +1,75 @@
 package com.amucss.mx.amucss;
 
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Registro_Activity extends AppCompatActivity {
+
+    Button bt_aviso;
+    Button btn_registra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // Rota horizontal
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Rota Vertical
         setContentView(R.layout.activity_registro_);
 
+        bt_aviso = (Button)findViewById(R.id.bt_aviso);
+        btn_registra = (Button)findViewById(R.id.btn_registra);
 
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Acepta el Aviso de Privacidad
+        bt_aviso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Registro_Activity.this);
+                builder.setTitle("Aviso de Privacidad");
+                builder.setMessage("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n" +
+                        "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.");
+                builder.setIcon(R.drawable.block);
+                builder.setPositiveButton("ACEPTAR",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
 
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                            }
+                        });
+
+                builder.setNegativeButton("CANCELAR",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                builder.show();
+
+
             }
-        });*/
+        });
+
+        // Guarda toda la Informacion
+        btn_registra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 
 }
